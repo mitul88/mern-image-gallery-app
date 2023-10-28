d883920c72d05951aefba71c77ef5e6cad5d8926
mitul88 <shahed.rahman88@gmail.com>
2023-10-28 23:46:04 +0600

    cors url changed

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors({
-        origin: 'http://localhost:3000'
+        origin: 'https://faithful-dog-gear.cyclic.app/'
     }));
     app.use(express.static('public'))
     app.use('*', cloudinaryConfig)
