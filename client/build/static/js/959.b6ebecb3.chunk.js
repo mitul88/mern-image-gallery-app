6f20d5b64f6a6b0e89b73a45b0abc3617619693a
mitul88 <shahed.rahman88@gmail.com>
2023-10-28 23:33:10 +0600

    commit build

---
+++ client/build/static/js/959.b6ebecb3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_gallery_client=self.webpackChunkimage_gallery_client||[]).push([[959],{7959:function(e,t,n){n.r(t),n.d(t,{action:function(){return a}});var r=n(1989);function a(){return localStorage.removeItem("token"),localStorage.removeItem("expiration"),(0,r.uX)("/")}}}]);
+//# sourceMappingURL=959.b6ebecb3.chunk.js.map
