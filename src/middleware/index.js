fcb258c9646234d272ab47beb30ef84e3913b1ae
mitul88 <shahed.rahman88@gmail.com>
2023-10-28 23:22:13 +0600

    bug fix

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -14,7 +14,16 @@ module.exports = (app) => {
     app.use(express.static('public'))
     app.use('*', cloudinaryConfig)
 
+    // for deployment static file serve
     app.use(express.static(path.join(__dirname, "../../client/build")));
+    app.get("/", function(req, res) {
+        res.sendFile(
+            path.join(__dirname, "../../client/build/index.html"),
+            function(err) {
+                res.status(500).send(err)
+            }
+        )
+    })
 
     if (ENV_CONFIG.node_env === 'development') {
         app.use(morgan('dev'));
